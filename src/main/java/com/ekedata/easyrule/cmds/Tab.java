53988f4e1ac45afff989f1dd6c53ce1747208dcf
小兴 <645710596@qq.com>
2024-01-26 18:44:19 +0800

    添加TAB指令辅助。

--- src/main/java/com/ekedata/easyrule/cmds/Tab.java
+++ src/main/java/com/ekedata/easyrule/cmds/Tab.java
@@ -22,6 +22,14 @@ public class Tab implements TabCompleter {
         if (args.length == 1) {
             completions.add("爆炸保护");
             completions.add("死亡不掉落");
+            completions.add("在公告区显示玩家进度");
+            completions.add("命令方块执行时显示");
+            completions.add("让服务器停止检查使用鞘翅的玩家的移动速度");
+            completions.add("是否禁用袭击");
+            completions.add("是否进行日夜交替和月相变化");
+            completions.add("非生物实体是否掉落物品");
+            completions.add("火是否蔓延及自然熄灭");
+            completions.add("幻翼是否在夜晚生成");
         }
         if (args.length == 2) {
             completions.add("true");
