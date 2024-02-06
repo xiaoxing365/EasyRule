8f9e93109b39bbac616e9b2f80de0d1846ca4fe4
小兴 <645710596@qq.com>
2024-02-06 21:05:47 +0800

    测试使用Vscode写

--- src/main/java/com/ekedata/easyrule/utils/SendUtil.java
+++ src/main/java/com/ekedata/easyrule/utils/SendUtil.java
@@ -2,22 +2,14 @@ package com.ekedata.easyrule.utils;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+// import org.jetbrains.annotations.NotNull;
+// import org.jetbrains.annotations.Nullable;
 
 public class SendUtil {
-    public static void sendToPlayer(@Nullable CustomColor color,@Nullable Player player,@NotNull String msg){
+    public static void sendToPlayer( CustomColor color, Player player, String msg){
         player.sendMessage(color+msg);
     }
 
-    public static void sendToConsole(@Nullable CustomColor color,@NotNull String msg){
+    public static void sendToConsole( CustomColor color, String msg){
         Bukkit.getConsoleSender().sendMessage(color+msg);
-    }
-    public static void sendPlayer(@Nullable Player player,@NotNull String msg){
-        player.sendMessage(msg);
-    }
-
-    public static void sendConsole(@NotNull String msg){
-        Bukkit.getConsoleSender().sendMessage(msg);
-    }
 }
